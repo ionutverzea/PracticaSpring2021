a97d422bb080a1b7bb021ac4aa7eee36961c233b
AnitaBoghea <Anita.Boghea@ctbav.ro>
2021-05-26 21:52:04 +0300

    Qt-implmentare interfata (toolbar+implemantare poza din file explorer)

---
+++ INTERFATA/build-ToolBar-Desktop_Qt_6_2_0_MinGW_64_bit-Debug/ToolBar_autogen/include/ui_mainwindow.h
@@ -0,0 +1,153 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.2.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionOpen;
+    QAction *actionSave;
+    QAction *actionSave_as;
+    QAction *actionRecent_files;
+    QAction *actionExit;
+    QAction *actionAbout;
+    QAction *actionInfo;
+    QWidget *centralwidget;
+    QLabel *label;
+    QMenuBar *menubar;
+    QMenu *menuFILE;
+    QMenu *menuEDIT;
+    QMenu *menuHELP;
+    QStatusBar *statusbar;
+    QToolBar *toolBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(800, 600);
+        actionOpen = new QAction(MainWindow);
+        actionOpen->setObjectName(QString::fromUtf8("actionOpen"));
+        QIcon icon;
+        icon.addFile(QString::fromUtf8("img/open.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionOpen->setIcon(icon);
+        actionSave = new QAction(MainWindow);
+        actionSave->setObjectName(QString::fromUtf8("actionSave"));
+        QIcon icon1;
+        icon1.addFile(QString::fromUtf8("img/save.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionSave->setIcon(icon1);
+        actionSave_as = new QAction(MainWindow);
+        actionSave_as->setObjectName(QString::fromUtf8("actionSave_as"));
+        QIcon icon2;
+        icon2.addFile(QString::fromUtf8("img/save as.jpg"), QSize(), QIcon::Normal, QIcon::Off);
+        actionSave_as->setIcon(icon2);
+        actionRecent_files = new QAction(MainWindow);
+        actionRecent_files->setObjectName(QString::fromUtf8("actionRecent_files"));
+        QIcon icon3;
+        icon3.addFile(QString::fromUtf8("img/recent file.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionRecent_files->setIcon(icon3);
+        actionExit = new QAction(MainWindow);
+        actionExit->setObjectName(QString::fromUtf8("actionExit"));
+        QIcon icon4;
+        icon4.addFile(QString::fromUtf8("img/exit.jpg"), QSize(), QIcon::Normal, QIcon::Off);
+        actionExit->setIcon(icon4);
+        actionAbout = new QAction(MainWindow);
+        actionAbout->setObjectName(QString::fromUtf8("actionAbout"));
+        QIcon icon5;
+        icon5.addFile(QString::fromUtf8("img/about.jpg"), QSize(), QIcon::Normal, QIcon::Off);
+        actionAbout->setIcon(icon5);
+        actionInfo = new QAction(MainWindow);
+        actionInfo->setObjectName(QString::fromUtf8("actionInfo"));
+        QIcon icon6;
+        icon6.addFile(QString::fromUtf8("img/info.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionInfo->setIcon(icon6);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(0, 0, 438, 348));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 26));
+        menuFILE = new QMenu(menubar);
+        menuFILE->setObjectName(QString::fromUtf8("menuFILE"));
+        menuEDIT = new QMenu(menubar);
+        menuEDIT->setObjectName(QString::fromUtf8("menuEDIT"));
+        menuHELP = new QMenu(menubar);
+        menuHELP->setObjectName(QString::fromUtf8("menuHELP"));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+        toolBar = new QToolBar(MainWindow);
+        toolBar->setObjectName(QString::fromUtf8("toolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, toolBar);
+
+        menubar->addAction(menuFILE->menuAction());
+        menubar->addAction(menuEDIT->menuAction());
+        menubar->addAction(menuHELP->menuAction());
+        menuFILE->addAction(actionOpen);
+        menuFILE->addAction(actionSave);
+        menuFILE->addAction(actionSave_as);
+        menuFILE->addAction(actionRecent_files);
+        menuFILE->addAction(actionExit);
+        menuHELP->addAction(actionAbout);
+        menuHELP->addAction(actionInfo);
+        toolBar->addAction(actionOpen);
+        toolBar->addAction(actionSave);
+        toolBar->addAction(actionSave_as);
+        toolBar->addAction(actionRecent_files);
+        toolBar->addAction(actionExit);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        actionOpen->setText(QCoreApplication::translate("MainWindow", "Open", nullptr));
+        actionSave->setText(QCoreApplication::translate("MainWindow", "Save", nullptr));
+        actionSave_as->setText(QCoreApplication::translate("MainWindow", "Save as", nullptr));
+        actionRecent_files->setText(QCoreApplication::translate("MainWindow", "Recent files", nullptr));
+        actionExit->setText(QCoreApplication::translate("MainWindow", "Exit", nullptr));
+        actionAbout->setText(QCoreApplication::translate("MainWindow", "About", nullptr));
+        actionInfo->setText(QCoreApplication::translate("MainWindow", "Info", nullptr));
+        label->setText(QString());
+        menuFILE->setTitle(QCoreApplication::translate("MainWindow", "FILE", nullptr));
+        menuEDIT->setTitle(QCoreApplication::translate("MainWindow", "EDIT", nullptr));
+        menuHELP->setTitle(QCoreApplication::translate("MainWindow", "HELP", nullptr));
+        toolBar->setWindowTitle(QCoreApplication::translate("MainWindow", "toolBar", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
